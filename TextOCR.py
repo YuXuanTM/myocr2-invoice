b8309447b5f2c03feb8c055da52f18c578993d45
zhengenyuan <zhengenyuan@idataway.com>
2025-07-01 10:47:07 +0800

    update

--- TextOCR.py
+++ TextOCR.py
@@ -1,25 +1,28 @@
-import os
 from paddleocr import PaddleOCR
 from paddleocr import TextRecognition
 from tool.public_info import device
 
 ocr = PaddleOCR(
-    device = device,
+    device=device,
     text_detection_model_name="PP-OCRv5_mobile_det",
     text_recognition_model_name="PP-OCRv5_mobile_rec",
     text_detection_model_dir='models/PP-OCRv5_mobile_det_infer',
     text_recognition_model_dir='models/PP-OCRv5_mobile_rec_infer',
-    # enable_mkldnn=True,
-    # mkldnn_cache_capacity=2048,
+    enable_mkldnn=True,
+    mkldnn_cache_capacity=5,  # 增加缓存容量
     use_doc_orientation_classify=False,
     use_doc_unwarping=False,
     use_textline_orientation=False
 )
-ocr_rec = TextRecognition(device = device,model_name = 'PP-OCRv5_mobile_rec', model_dir=r"models/PP-OCRv5_mobile_rec_infer")
+ocr_rec = TextRecognition(
+    mkldnn_cache_capacity=5,
+    enable_mkldnn=True,
+    device=device, model_name='PP-OCRv5_mobile_rec',
+    model_dir=r"models/PP-OCRv5_mobile_rec_infer")
 
 
-def predict(img, lock ='rec'):
-  if lock == 'ocr':
-    return ocr.predict(img, )
-  else:
-    return ocr_rec.predict_iter(img)
+def predict(img, lock='rec'):
+    if lock == 'ocr':
+        return ocr.predict(img)
+    else:
+        return ocr_rec.predict_iter(img)
--- predict2.py
+++ predict2.py
@@ -25,69 +25,73 @@ model2 = Detector(
 labels = model2.pred_config.labels
 
 
-
 def start(processed_img):
-  start_time = time.perf_counter()
-  img = np.array(processed_img).astype(np.uint8)
-  results = model2.predict_image([img], visual=False)
-  boxes = {}
-  item_boxes = []
-  items = []
-  for e in results['boxes']:
-    class_id, confidence, left, top, right, bottom = e
-    if confidence < confidence_threshold:
-      continue
-    label = labels[int(class_id)]
-    n = (left, top, right, bottom, label, confidence)
-    if label == 'item':
-     set_box(item_boxes, n)
-     continue
-    elif label.startswith('item_'):
-      set_box(items, n)
-      continue
-    else:
-      boxe = boxes.get(label, None)
-      if boxe is None or boxe[5] < confidence:
-        boxe = [left, top, right, bottom, label, confidence]
-        boxes[label] = boxe
-        continue
-  spatial_index, boxs = create_spatial_index(item_boxes)
-  item_info = {}
-  for item in items:
-    point = ((item[0] + item[2]) / 2, (item[1] + item[3]) / 2)
-    i = point_in_boxes(point, boxs, spatial_index)
-    if i is not None:
-      if i in item_info:
-        item_info[i].append(item)
-      else:
-        item_info[i] = [item]
-  end_time  = time.perf_counter()
-  execution_time = end_time - start_time
-  print(f"检测耗时: {execution_time} 秒")
-  return boxes.values(), item_info.values(), item_boxes, items
+    start_time = time.perf_counter()
+    img = np.array(processed_img).astype(np.uint8)
+    results = model2.predict_image([img], visual=False)
+    boxes = {}
+    item_boxes = []
+    items = []
+    for e in results['boxes']:
+        class_id, confidence, left, top, right, bottom = e
+        if confidence < confidence_threshold:
+            continue
+        label = labels[int(class_id)]
+        n = (left, top, right, bottom, label, confidence)
+        if label == 'item':
+            set_box(item_boxes, n)
+            continue
+        elif label.startswith('item_'):
+            set_box(items, n)
+            continue
+        else:
+            boxe = boxes.get(label, None)
+            if boxe is None or boxe[5] < confidence:
+                boxe = [left, top, right, bottom, label, confidence]
+                boxes[label] = boxe
+                continue
+    spatial_index, boxs = create_spatial_index(item_boxes)
+    item_info = {}
+    for item in items:
+        point = ((item[0] + item[2]) / 2, (item[1] + item[3]) / 2)
+        i = point_in_boxes(point, boxs, spatial_index)
+        if i is not None:
+            if i in item_info:
+                item_info[i].append(item)
+            else:
+                item_info[i] = [item]
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"检测耗时: {execution_time} 秒")
+    return boxes.values(), item_info.values(), item_boxes, items
+
 
 def set_box(item_boxes, n):
-  new_box = None
-  for box in item_boxes:
-    new_box = _get_box(n, box)
+    new_box = None
+    for box in item_boxes:
+        new_box = _get_box(n, box)
+        if new_box is not None:
+            break
     if new_box is not None:
-      break
-  if new_box is not None:
-    if new_box[5] >= n[5]:
-      return
-    new_box[0] = n[0]
-    new_box[1] = n[1]
-    new_box[2] = n[2]
-    new_box[3] = n[3]
-    new_box[4] = n[4]
-    new_box[5] = n[5]
-    # new_box = [left, top, right, bottom, label, confidence]
-  else:
-    item_boxes.append(n)
+        if new_box[5] >= n[5]:
+            return
+        new_box[0] = n[0]
+        new_box[1] = n[1]
+        new_box[2] = n[2]
+        new_box[3] = n[3]
+        new_box[4] = n[4]
+        new_box[5] = n[5]
+        # new_box = [left, top, right, bottom, label, confidence]
+    else:
+        item_boxes.append(n)
+
+
 def _get_box(box1, box2):
-  if not (box2[2] < box1[0] or box2[0] > box1[2] or box2[3] < box1[1] or box2[1] > box1[3]) :
-    return box2
-  else: return None
+    if not (box2[2] < box1[0] or box2[0] > box1[2] or box2[3] < box1[1] or box2[1] > box1[3]):
+        return box2
+    else:
+        return None
+
 
 def create_spatial_index(boxes):
     """
@@ -102,14 +106,14 @@ def create_spatial_index(boxes):
     boxs = []
     idx = index.Index()
     for i, (left, top, right, bottom, label, confidence) in enumerate(boxes):
-      # 插入边界框到索引中
-      boxs.append((left, top, right, bottom))
-      idx.insert(i, (left, top, right, bottom))
+        # 插入边界框到索引中
+        boxs.append((left, top, right, bottom))
+        idx.insert(i, (left, top, right, bottom))
     return idx, boxs
 
 
 def point_in_boxes(point, boxes, spatial_index):
-  """
+    """
   使用空间索引快速查找包含给定坐标的边界框。
 
   参数:
@@ -120,9 +124,9 @@ def point_in_boxes(point, boxes, spatial_index):
   返回:
   如果点位于某个边界框内，则返回该边界框；否则返回None。
   """
-  p = Point(point)
-  for i in spatial_index.intersection((point[0], point[1], point[0], point[1])):
-    b = Box(*boxes[i])
-    if p.within(b):
-      return i
-  return None
+    p = Point(point)
+    for i in spatial_index.intersection((point[0], point[1], point[0], point[1])):
+        b = Box(*boxes[i])
+        if p.within(b):
+            return i
+    return None
